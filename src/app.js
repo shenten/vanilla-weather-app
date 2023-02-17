fc9c58e889f36361178f4486f0e34a3d1fd52742
shenten <shan-26@hotmail.co.uk>
2023-02-17 13:51:30 +0000

    added currnet time to JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp){
+    let date = new Date();
+    let hours = date.getHours();
+     if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" ]
+    let day = days[date.getDay()]; 
+    return `${day} ${hours}:${minutes}`;
+    
+}
+
+
 
 function displayTemperature(response){
     console.log(response);
@@ -10,10 +27,13 @@ function displayTemperature(response){
    let humidityElement = document.querySelector("#humidity");
    humidityElement.innerHTML = response.data.temperature.humidity;
    let windElement = document.querySelector("#wind");
-   windElement.innerHTML = response.data.wind.speed.;
+   windElement.innerHTML = response.data.wind.speed;
+   let dateElement = document.querySelector("#date");
+   dateElement.innerHTML = formatDate(response.data.time * 1000);
+    console.log(response.data.time);
 }
 
 apiKey = `t95eob0fafd730717b08ab0a804ec543`;
-apiUrl = `https://api.shecodes.io/weather/v1/current?query=${"london"}&key=${apiKey}&units=metric`;
+apiUrl = `https://api.shecodes.io/weather/v1/current?query=${"erbil"}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 
